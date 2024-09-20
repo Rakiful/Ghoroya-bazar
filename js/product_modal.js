faeae391bc6d0e641ce60729bd5b25532cb190d2
rakiful <mr143131@gmail.com>
2024-09-20 22:45:16 +0600

    footer update

--- js/product_modal.js
+++ js/product_modal.js
@@ -133,17 +133,3 @@ document.getElementById("modalQuantity").addEventListener('keyup', () => {
     updateCart(getProductPrice());
 });
 
-
-//facebook Share
-document.getElementById('fbShareButton').addEventListener('click', function () {
-    // Get the modal content dynamically
-    const modalImage = document.getElementById('modalMainImage').src; // Modal main image
-    const modalProductName = document.getElementById('modalProductName').innerText; // Modal product name (caption)
-    const modalUrl = window.location.href; // Current URL (with modal open)
-
-    // Create Facebook Share URL
-    const fbShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(modalUrl)}&quote=${encodeURIComponent(modalProductName)}&picture=${encodeURIComponent(modalImage)}`;
-
-    // Open the Facebook share dialog in a new window
-    window.open(fbShareUrl, '_blank', 'width=600,height=400');
-});
